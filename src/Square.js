2e46d1280f2611acd492dcd9b4782b985122f3ee
GreggtheDev <greggthedev@gmail.com>
2024-07-09 22:17:51 -0400

    added notation

--- src/Square.js
+++ src/Square.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from "react"; // Importing React to create and manage components
 
+// Function component Square that receives 'value' and 'onClick' as props
 function Square({ value, onClick }) {
   return (
+    // Renders a button element with the class "square" and the onClick handler
     <button className="square" onClick={onClick}>
-      {value}
+      {value}{" "}
+      {/* Displays the value (either 'X', 'O', or null) inside the button */}
     </button>
   );
 }
 
-export default Square;
+export default Square; // Exports the Square component to be used in other parts of the application
